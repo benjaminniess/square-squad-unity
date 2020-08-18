64e48113eeb9de5cf21cba488a316d2d2d0d9120
bniess <bniess@sevanova.com>
2020-08-18 22:11:51 +0200

    Saved WIP

--- Assets/Scripts/PlayerMovements.cs
+++ Assets/Scripts/PlayerMovements.cs
@@ -2,43 +2,60 @@
 
 public class PlayerMovements : MonoBehaviour
 {
-	public float moveSpeed;
-	public float bouceForce;
-	public bool isGrounded;
-	public Transform groundCheckLeft;
-	public Transform groundCheckRight;
-	public Rigidbody2D rb;
-	private Vector3 velocity = Vector3.zero;
+    public float moveSpeed;
+    public float jumpForce;
 
-	//public Animator animator;
-	//public SpriteRenderer spriteRenderer;
+    private bool isJumping;
+    private bool isGrounded;
+
+    public Transform groundCheckLeft;
+    public Transform groundCheckRight;
+
+    public Rigidbody2D rb;
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+
+    private Vector3 velocity = Vector3.zero;
 
     void FixedUpdate()
     {
-    	isGrounded = Physics2D.OverlapArea(groundCheckLeft.position, groundCheckRight.position);
+        isGrounded = Physics2D.OverlapArea(groundCheckLeft.position, groundCheckRight.position);
+
         float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            isJumping = true;
+        }
 
         MovePlayer(horizontalMovement);
 
         Flip(rb.velocity.x);
 
-        //animator.SetBool("IsUp", !isGrounded);
+        float characterVelocity = Mathf.Abs(rb.velocity.x);
+        animator.SetFloat("Speed", characterVelocity);
     }
 
-	void MovePlayer(float _horizontalMovement) {
-		Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
-		rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
+    void MovePlayer(float _horizontalMovement)
+    {
+        Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
 
-		if ( isGrounded == true ) {
-			rb.AddForce(new Vector2(0f, bouceForce));
-		}
-	}
+        if(isJumping == true)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce));
+            isJumping = false;
+        }
+    }
 
-	void Flip(float _velocity) {
-		if ( _velocity > 0.1f ) {
-			//spriteRenderer.flipX = false;
-		} else if ( _velocity < -0.1f ) {
-			//spriteRenderer.flipX = true;
-		}
-	}
+    void Flip(float _velocity)
+    {
+        if (_velocity > 0.1f)
+        {
+            spriteRenderer.flipX = false;
+        }else if(_velocity < -0.1f)
+        {
+            spriteRenderer.flipX = true;
+        }
+    }
 }
