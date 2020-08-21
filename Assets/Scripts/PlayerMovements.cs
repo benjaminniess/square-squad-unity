e0fd175817070fa2dfaef4b09192058ea34fc389
Benjamin Niess <bniess@sevanova.com>
2020-08-21 13:57:51 +0200

    Added shit code

--- Assets/Scripts/PlayerMovements.cs
+++ Assets/Scripts/PlayerMovements.cs
@@ -20,11 +20,22 @@ public class PlayerMovements : MonoBehaviour
     public Animator animator;
     public SpriteRenderer spriteRenderer;
 
+    private Vector2 resetPos;
+
     private Vector3 velocity = Vector3.zero;
 
+    void Start() {
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+    }
     void FixedUpdate()
     {
         isGrounded = Physics2D.OverlapArea(groundCheckLeft.position, groundCheckRight.position);
+
+        if (Input.touchCount > 2) {
+            resetPos = new Vector2(-98.69f,3.63f );
+            rb.transform.position = resetPos;
+            rb.rotation = 0f;
+        }
 
         currentVector = topCenterPoint.transform.position - bottomCenterPoint.transform.position;
         if ( IsPushingLeft() ) {
@@ -32,6 +43,13 @@ public class PlayerMovements : MonoBehaviour
         }
         if ( IsPushingRight() ) {
             rightReactor.AddForce(currentVector * reactorForce * Time.deltaTime);
+        }
+
+        if ( Input.GetKey("up") ) {
+            Debug.Log("Reset");
+            resetPos = new Vector2(-98.69f,3.63f );
+            rb.transform.position = resetPos;
+            rb.rotation = 0f;
         }
     
 
@@ -43,10 +61,44 @@ public class PlayerMovements : MonoBehaviour
     }
 
     static bool IsPushingRight() {
+        if (Input.touchCount > 0) {
+            Touch touch = Input.GetTouch(0);
+            Vector2 pos = touch.position;
+            if ( pos.x > ( Screen.width / 2 ) ) {
+                return true;
+            } else {
+                if (Input.touchCount > 1) {
+                    Touch touch2 = Input.GetTouch(1);
+                    Vector2 pos2 = touch2.position;
+                    if ( pos2.x > ( Screen.width / 2 ) ) {
+                        return true;
+                    } else {
+                        return false; 
+                    }
+                }
+            }
+        }
         return Input.GetKey("right");
     }
 
     static bool IsPushingLeft() {
+        if (Input.touchCount > 0) {
+            Touch touch = Input.GetTouch(0);
+            Vector2 pos = touch.position;
+            if ( pos.x < ( Screen.width / 2 ) ) {
+                return true;
+            } else {
+            if (Input.touchCount > 1) {
+                Touch touch3 = Input.GetTouch(1);
+                Vector2 pos3 = touch3.position;
+                if ( pos3.x < ( Screen.width / 2 ) ) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+            }
+        }
         return Input.GetKey("left");
     }
 
