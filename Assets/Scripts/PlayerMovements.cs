c5e7d689cb7f36d8b1b6e7e99f32b2fd12d97262
Benjamin Niess <benjamin.niess@gmail.com>
2020-08-25 22:39:50 +0200

    Removed lots of stuff

--- Assets/Scripts/PlayerMovements.cs
+++ Assets/Scripts/PlayerMovements.cs
@@ -5,27 +5,13 @@ using TMPro;
 public class PlayerMovements : MonoBehaviour
 {
     public float reactorForce;
-
-    private bool isGrounded;
-
-    public Transform groundCheckLeft;
-    public Transform groundCheckRight;
-
-    public Rigidbody2D rightReactor;
-    public Rigidbody2D leftReactor;
-
     public Joystick leftJoystick;
     public Joystick rightJoystick;
-
     public Rigidbody2D rb;
-
     public GameObject topCenterPoint;
     public Rigidbody2D bottomCenterPoint;
-    public Vector2 currentVectorLeft;
-    public Vector2 currentVectorRight;
 
-    
-
+    private Vector2 currentVector;
     private Vector3 lastPosition = Vector3.zero;
     private float speed;
     private float rotationSpeed;
@@ -53,40 +39,32 @@ public class PlayerMovements : MonoBehaviour
         rotationLast = currentRotation;
         rotationDelta = rotationDelta < 0 ? -rotationDelta : rotationDelta;
 
-        isGrounded = Physics2D.OverlapArea(groundCheckLeft.position, groundCheckRight.position);
-
-        currentVectorRight = ( topCenterPoint.transform.position - groundCheckRight.transform.position ).normalized;
-        currentVectorLeft = ( topCenterPoint.transform.position - groundCheckLeft.transform.position ).normalized;
+        currentVector = ( topCenterPoint.transform.position - bottomCenterPoint.transform.position  ).normalized;
 
         float rightReactorLevel = getRightReactorLevel();
         float leftReactorLevel = getLeftReactorLevel();
 
         if ( rightReactorLevel == 0 && leftReactorLevel == 0 ) {
-            //Debug.Log(currentRotation);
-            //rb.MoveRotation( 0f - (currentRotation / 4500 ) * Time.deltaTime);
-            //rb.AddTorque(-currentRotation * Time.deltaTime * 10); 
-            //rb.velocity = rb.velocity * 0.8f; 
+            // No joystick touched?
         }
-
         
+        float torqueLevel = 0;
         if ( rightReactorLevel > 0 ) {
-            rightReactor.AddForce(currentVectorRight * reactorForce * Time.deltaTime * rightReactorLevel); 
+            torqueLevel += rightReactorLevel;
+            bottomCenterPoint.AddForce(currentVector * reactorForce * Time.deltaTime * rightReactorLevel);
         } else if ( rightReactorLevel < 0 ) {
             rb.AddTorque(Time.deltaTime * rightReactorLevel * 600);
         }
 
         if ( leftReactorLevel > 0 ) {
-            leftReactor.AddForce(currentVectorLeft * reactorForce * Time.deltaTime * leftReactorLevel);
+            torqueLevel -= leftReactorLevel;
+            bottomCenterPoint.AddForce(currentVector * reactorForce * Time.deltaTime * leftReactorLevel);
         } else if ( leftReactorLevel < 0 ) {
             rb.AddTorque(Time.deltaTime * -leftReactorLevel * 600);
         }
-    
-        bottomCenterPoint.gravityScale = -0.55f;  
-        
 
-        
-        //rightReactor.AddForce( new Vector2( bottomCenterPoint.transform.position, groundCheckRight.transform.position ).normalized * (reactorForce / 10) * Time.deltaTime * rightJoystick.Horizontal);
-        //leftReactor.AddForce( new Vector2( bottomCenterPoint.transform.position, groundCheckLeft.transform.position ).normalized * (reactorForce / 10) * Time.deltaTime * leftJoystick.Horizontal);
+        Debug.Log(speed);
+        rb.AddTorque(torqueLevel * Time.deltaTime * 300);
     }
 
     float getRightReactorLevel() {
