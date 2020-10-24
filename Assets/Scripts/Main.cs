cab081ceb6ab0a7e7ae759614ee8ccce789386a2
Benjamin Niess <benjamin.niess@gmail.com>
2020-10-24 14:38:50 +0200

    Dynamically added players to scene

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -8,8 +8,10 @@ public class Main : MonoBehaviour
     public static Main instance;
 
     public GameObject Coin;
+    public GameObject Player;
 
-    public GameObject[] PlayersObjects;
+    private GameObject[] playersScores;
+    private GameObject[] PlayersObjects;
 
     public GameObject[] BonusTypes;
 
@@ -28,13 +30,61 @@ public class Main : MonoBehaviour
     {
         GenerateCoin();
         GenerateBonus();
-       // GenerateBonus();
-        PlayersObjects = GameObject.FindGameObjectsWithTag("Player");
+        GenerateBonus();
+        GeneratePlayers();
     }
 
     // Update is called once per frame
     void Update() {
         UpdateScores();
+    }
+
+    public void GeneratePlayers() {
+        playersScores = new GameObject[4];
+        for ( int i = 0; i < 4; i++) {
+            playersScores[i] = GameObject.Find("ScorePlayer" + (i+1));
+            playersScores[i].SetActive(false);
+        }
+
+        for ( int i = 1; i <= 2; i++) {
+            GameObject spawnPosition =  GameObject.Find("SpawnPositionForPlayer" + i);
+            GameObject PlayerObject = Instantiate(Player, spawnPosition.transform.position, Quaternion.identity);
+            PlayerMovements playerScript = PlayerObject.GetComponent<PlayerMovements>();
+            playersScores[i-1].SetActive(true);
+
+            if ( i == 1 ) {
+                playerScript.upTouch = "z";
+                playerScript.leftTouch = "q";
+                playerScript.rightTouch = "d";
+                playerScript.downTouch = "s";
+                playerScript.dashTouch = "g";
+                playerScript.bonusTouch = "f";
+            } else if ( i == 2 ) {
+                playerScript.upTouch = "up";
+                playerScript.leftTouch = "left";
+                playerScript.rightTouch = "right";
+                playerScript.downTouch = "down";
+                playerScript.dashTouch = "n";
+                playerScript.bonusTouch = "b";
+            } else if ( i == 3 ) {
+                playerScript.upTouch = "u";
+                playerScript.leftTouch = "h";
+                playerScript.rightTouch = "k";
+                playerScript.downTouch = "j";
+                playerScript.dashTouch = "l";
+                playerScript.bonusTouch = "m";
+            } else if ( i == 4 ) {
+                playerScript.upTouch = "w";
+                playerScript.leftTouch = "x";
+                playerScript.rightTouch = "c";
+                playerScript.downTouch = "v";
+                playerScript.dashTouch = "p";
+                playerScript.bonusTouch = "m";
+            }
+        }
+
+        PlayersObjects = GameObject.FindGameObjectsWithTag("Player");
+        
     }
 
     public void GenerateCoin() {
@@ -58,12 +108,11 @@ public class Main : MonoBehaviour
     }
 
     public void UpdateScores() {
-        int i = 1;
+        int i = 0;
         foreach ( GameObject Player in PlayersObjects ) {
             PlayerMovements playerScript = Player.GetComponent<PlayerMovements>();
-            GameObject.Find("ScorePlayer" + i).GetComponent<TMPro.TextMeshProUGUI>().SetText(playerScript.getScore().ToString());
+            playersScores[i].GetComponent<TMPro.TextMeshProUGUI>().SetText(playerScript.getScore().ToString());
             i++;
-        }
-        
+        }   
     }
 }
