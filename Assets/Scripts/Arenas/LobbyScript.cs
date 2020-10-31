1d93b503d2e8103af7c146654583fbb8f255d2c4
bniess <bniess@sevanova.com>
2020-10-31 16:15:19 +0100

    Improved lobby players management

--- Assets/Scripts/Arenas/LobbyScript.cs
+++ Assets/Scripts/Arenas/LobbyScript.cs
@@ -16,24 +16,35 @@ public class LobbyScript : MonoBehaviour
 
     private void Awake()
     {
-        // if the singleton hasn't been initialized yet
-        if (instance != null && instance != this)
-        {
-            Destroy(this.gameObject);
-        }
-
-        instance = this;
+        if(instance == null){
+             DontDestroyOnLoad(gameObject);
+             instance = this;
+         }
+         else{
+             if(instance != this){
+                 Destroy (gameObject);
+             }
+         }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        GeneratePlayers();
+        
+    }
+
+    public void reset() {
         ResetPlayers();
     }
 
     public void initPlayer(PlayerMovements playerScript)
     {
+        Scene scene = SceneManager.GetActiveScene();
+        if ( scene.name != "Lobby" ) {
+            Destroy(playerScript.gameObject);
+            return;
+        }
+
         PlayerCount++;
 
         GameObject spawnPosition = GameObject.Find("SpawnPositionForPlayer" + PlayerCount);
@@ -51,11 +62,28 @@ public class LobbyScript : MonoBehaviour
             playerScript.setColor(new Color(88f/255f, 109f/255f, 245f/255f));
         }
         DontDestroyOnLoad(playerScript);
-        GeneratePlayers();
+
+        int ArraySize = PlayersObjects == null ? 1 : PlayersObjects.Length + 1;
+        GameObject[] save = PlayersObjects;
+        PlayersObjects = new GameObject[ArraySize];
+        int i = 0;
+
+        if ( save != null ) {
+            foreach (GameObject Player in save)
+            {
+                PlayersObjects[i] = Player;
+                i++;
+            }
+        }
+        
+        PlayersObjects[i] = playerScript.gameObject;
     }
 
     public void ResetPlayers() {
         int playerReCount = 1;
+        if ( PlayersObjects == null ) {
+            return;
+        }
         foreach (GameObject Player in PlayersObjects)
         {
             PlayerMovements playerScript = Player.GetComponent<PlayerMovements>();
@@ -67,14 +95,8 @@ public class LobbyScript : MonoBehaviour
         }
     }
 
-    public void GeneratePlayers()
-    {
-        PlayersObjects = GameObject.FindGameObjectsWithTag("Player");
-    }
-
     public GameObject[] getPlayers()
     {
         return PlayersObjects;
     }
-
 }
