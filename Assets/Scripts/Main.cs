7cc8a541535d4e3bf0f5c3de72e446263121d68c
bniess <bniess@sevanova.com>
2020-10-31 10:01:29 +0100

    Improved lobby scene

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -35,7 +35,6 @@ public class Main : MonoBehaviour
         map = FindObjectOfType<Tilemap>();
         
         GenerateCoin();
-        GenerateCoin();GenerateCoin();GenerateCoin();GenerateCoin();GenerateCoin();GenerateCoin();GenerateCoin();GenerateCoin();
         GenerateBonus();
         GenerateBonus();
         GeneratePlayers();
